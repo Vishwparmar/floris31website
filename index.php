487a66ff56f26b7f0aa960865b3f91cec8d3d146
shah-dishaa <151530660+shah-dishaa@users.noreply.github.com>
2023-11-20 22:56:14 +0530

    Update index.php
    Management added

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
 <body>
         <nav class="navbar navbar-expand-lg navbar-light bg-white px-lg-3 py-lg-2 shadow-sm sticky-top">
         <div class="container-fluid">
-            <a class="navbar-brand me-5 fw-bold fs-3 h-font" href="index.php">Floris 31 resort</a>
+            <a class="navbar-brand me-5 fw-bold fs-3 h-font" href="index.php">Floris31 Resort Management</a>
             <button class="navbar-toggler shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
