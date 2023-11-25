b5de8351d2b58cee4bb5ae9f86a148b89c057325
shah-dishaa <dishashah071@gmail.com>
2023-11-25 20:00:26 +0530

    modified

--- include/footer.php
+++ include/footer.php
@@ -1,5 +1,5 @@
 <div class="container-fluid">
-<div class="row">
+    <div class="row">
         <div class="col-lg-4 p-4">
             <h3 class="h-font fw-bold fs-3">Floris31 Resort </h3>
             <p>
@@ -26,7 +26,7 @@
                 <i class="bi bi-instagram me-1"></i>Instagram
             </a><br>
         </div>
-</div>
+    </div>
 </div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
