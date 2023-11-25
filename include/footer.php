7aff68783d6c11b5be42b94afd56f08f320cf2f0
nupur0309 <nupur.cnj@gmail.com>
2023-11-25 18:48:05 +0530

    Include file Added

---
+++ include/footer.php
@@ -0,0 +1,32 @@
+<div class="container-fluid">
+<div class="row">
+        <div class="col-lg-4 p-4">
+            <h3 class="h-font fw-bold fs-3">Floris31 Resort </h3>
+            <p>
+                The website aims to provide an efficient and user-friendly way for the users to check and reserves rooms for a hassle-free tour as well as for the resort staff to interact with customers to improve their satisfaction.
+            </p>
+        </div>
+        <div class="col-lg-4 p-4">
+            <h5 class="mb-3">Links</h5>
+            <a href="#" class="text-decoration-none d-inline-block mb-2 text-dark">Home</a><br>
+            <a href="#" class="text-decoration-none d-inline-block mb-2 text-dark">Rooms</a><br>
+            <a href="#" class="text-decoration-none d-inline-block mb-2 text-dark">Facilities</a><br>
+            <a href="#" class="text-decoration-none d-inline-block mb-2 text-dark">Contact Us</a><br>
+            <a href="#" class="text-decoration-none d-inline-block mb-2 text-dark">About</a><br>
+        </div>
+        <div class="col-lg-4 p-4">
+            <h5 class="mb-3">Follow us</h5>
+            <a href="#" class="d-inline-block text-dark text-decoration-none mb-2">
+                <i class="bi bi-twitter me-1"></i>Twitter
+            </a><br>
+            <a href="#" class="d-inline-block text-dark text-decoration-none mb-2">
+                <i class="bi bi-facebook me-1"></i>Facebook
+            </a><br>
+            <a href="#" class="d-inline-block text-dark text-decoration-none mb-2">
+                <i class="bi bi-instagram me-1"></i>Instagram
+            </a><br>
+        </div>
+</div>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
