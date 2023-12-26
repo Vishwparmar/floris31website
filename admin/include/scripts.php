3d5ad730f7996b789278d66e833eeeb271757916
shah-dishaa <dishashah071@gmail.com>
2023-12-26 19:49:36 +0530

    dynamic frontend added

--- admin/include/scripts.php
+++ admin/include/scripts.php
@@ -1,4 +1,5 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
 <script>
     function alert(type,msg){
         let bs_class = (type == 'success') ? 'alert-success' : 'alert-danger';
@@ -9,4 +10,20 @@
             </div>`;
         document.body.append(element);
     }
+
+    function setActive() 
+    {
+        let navbar = document.getElementById('#dashboard-menu');
+        let a_tags= document.getElementsByTagName('a');
+
+        for (i=0; i<a_tags.length; i++){
+            let file = a_tags[i].href.split('/').pop();
+            let file_name = file.split('.')[0];
+
+            if(document.location.href.indexOf(file_name) >= 0){
+                a_tags[i].classList.add('active');
+            }
+        }
+    }
+    setActive();
 </script>
--- index.php
+++ index.php
@@ -23,27 +23,28 @@
 </head>
 <body class="bg-light"><!--navbar-->
     <?php require('include/header.php'); ?>
+
     <!-- Carousel -->
     <div class="container-fluid px-lg-4 mt-4">
         <div style="--swiper-navigation-color: #fff; --swiper-pagination-color: #fff" class="swiper mySwiper2">
                 <div class="swiper-wrapper">
                 <div class="swiper-slide">
-                    <img src="images/pic1.avif" class="w-100 d-block" height="650px"/>
+                    <img src="images/pic1.avif" class="w-100 d-block" height="480px"/>
                 </div>
                 <div class="swiper-slide">
-                    <img src="images/pic2.jpg" class="w-100 d-block" height="650px"/>
+                    <img src="images/pic2.jpg" class="w-100 d-block" height="480px"/>
                 </div>
                 <div class="swiper-slide">
-                    <img src="images/room.webp" class="w-100 d-block" height="650px"/>
+                    <img src="images/room.webp" class="w-100 d-block" height="480px"/>
                 </div>
                 <div class="swiper-slide">
-                    <img src="images/pic4.jpg" class="w-100 d-block" height="650px"/>
+                    <img src="images/pic4.jpg" class="w-100 d-block" height="480px"/>
                 </div>
                 <div class="swiper-slide">
-                  <img src="images/pic5.jpg" class="w-100 d-block" height="650px"/>
+                  <img src="images/pic5.jpg" class="w-100 d-block" height="480px"/>
                 </div>
                 <div class="swiper-slide">
-                    <img src="images/pic10.jpg" class="w-100 d-block" height="650px"/>
+                    <img src="images/pic10.jpg" class="w-100 d-block" height="480px"/>
                 </div>
                 </div>
             <div class="swiper-button-next"></div>
@@ -93,7 +94,7 @@
 
 
     <!-- Our Rooms -->
-    <h2 class="mt-5 pt-4 md-4 text-center fw-bold h-font">OUR ROOMS</h2>
+    <h2 class="mt-5 pt-4 md-4 text-center fw-bold h-font">OUR ROOMS</h2><br>
     
     <div class="container">
         <div class="row">
@@ -418,39 +419,53 @@
     </div>
 
     <!-- Reach us -->
-    <h2 class="mt-5 pt-4 md-4 text-center fw-bold h-font">REACH US</h2>
+
+    <h2 class="mt-5 pt-4 md-4 text-center fw-bold h-font">REACH US</h2><br>
 
     <div class="container">
         <div class="row">
             <div class="col-lg-8 col-md-8 p-4 mb-lg-0 mb-3 bg-white rounded">
-                <iframe class="w-100 rounded" height="350" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d235014.2991840631!2d72.41493225790053!3d23.02015808674564!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395e848aba5bd449%3A0x4fcedd11614f6516!2sAhmedabad%2C%20Gujarat!5e0!3m2!1sen!2sin!4v1703395980990!5m2!1sen!2sin" loading="lazy"></iframe>
+                <iframe class="w-100 rounded" height="320px" src="<?php echo $contact_r['iframe'] ?>" loading="lazy"></iframe>
             </div>
             <div class="col-lg-4 col-md-4">
                 <div class="bg-white p-4 rounded mb-4">
                     <h5>Call us</h5>
-                    <a href="tel: +911234567890" class="d-inline-block mb-2 text-decoration-none text-dark">
-                        <i class="bi bi-telephone-fill"></i> +911234567890
+                    <a href="tel: +<?php echo $contact_r['pn1'] ?>" class="d-inline-block mb-2 text-decoration-none text-dark">
+                        <i class="bi bi-telephone-fill"></i> +<?php echo $contact_r['pn1'] ?>
                     </a>
                     <br>
-                    <a href="tel: +911234567890" class="d-inline-block text-decoration-none text-dark">
-                        <i class="bi bi-telephone-fill"></i> +911234567890
-                    </a>
+                    <?php 
+                        if($contact_r['pn2']!=''){
+                            echo<<<data
+                                <a href="tel: +$contact_r[pn2]" class="d-inline-block text-decoration-none text-dark">
+                                    <i class="bi bi-telephone-fill"></i> +$contact_r[pn2]
+                                </a>
+                            data;
+                        }
+                    ?>
                 </div>
                 <div class="bg-white p-4 rounded mb-4">
                     <h5>Follow us</h5>
-                    <a href="#" class="d-inline-block mb-3">
-                        <span class="badge bg-light text-dark fs-6 p-2">
-                            <i class="bi bi-twitter me-1"></i> Twitter
-                        </span>
-                    </a>
-                    <br>
-                    <a href="#" class="d-inline-block mb-3">
+                    <?php 
+                        if($contact_r['tw']!=''){
+                            echo<<<data
+                                <a href="$contact_r[tw]" class="d-inline-block mb-3">
+                                    <span class="badge bg-light text-dark fs-6 p-2">
+                                    <i class="bi bi-twitter me-1"></i> Twitter
+                                    </span>  
+                                </a>
+                                <br>
+                            data;
+                        }
+                    ?>
+                    
+                    <a href="<?php echo $contact_r['fb'] ?>" class="d-inline-block mb-3">
                         <span class="badge bg-light text-dark fs-6 p-2">
                             <i class="bi bi-facebook"></i> Facebook
                         </span>
                     </a>
                     <br>
-                    <a href="#" class="d-inline-block">
+                    <a href="<?php echo $contact_r['insta'] ?>" class="d-inline-block">
                         <span class="badge bg-light text-dark fs-6 p-2">
                             <i class="bi bi-instagram"></i> Instagram
                         </span>
