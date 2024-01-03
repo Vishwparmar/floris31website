75f2ca01d2c989bd6fad587441d1225d1043203b
shah-dishaa <dishashah071@gmail.com>
2024-01-03 17:49:10 +0530

    managrment team section added

--- admin/include/db_config.php
+++ admin/include/db_config.php
@@ -22,6 +22,12 @@
         return $data;
     }
 
+    function selectAll($table){
+        $con = $GLOBALS['con'];
+        $res = mysqli_query($con,"SELECT * FROM $table");
+        return $res;
+    }
+
     function select($sql,$values,$datatypes){
         $con = $GLOBALS['con'];
         if($stmt = mysqli_prepare($con,$sql)){
