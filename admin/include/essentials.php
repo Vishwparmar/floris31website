d8ac8468fc7223be4812c9f3ae443808dd509856
VishwaParmar <vishwaparmar169@gmail.com>
2024-01-05 13:52:48 -0800

    Features and facilities backend done

--- admin/include/essentials.php
+++ admin/include/essentials.php
@@ -3,11 +3,13 @@
 
     define('SITE_URL','http://127.0.0.1/floris31website/');
     define('ABOUT_IMG_PATH',SITE_URL.'images/about/');
+    define('FACILITIES_IMG_PATH',SITE_URL.'images/facilities/');
 
     //backend upload process needs this data
 
     define('UPLOAD_IMAGE_PATH',$_SERVER['DOCUMENT_ROOT'].'/floris31website/images/');
     define('ABOUT_FOLDER','about/');
+    define('FACILITIES_FOLDER','facilities/');
 
     function adminLogin(){
         session_start();
@@ -74,4 +76,34 @@
             return false;
         }
     }
+
+
+    function uploadSVGImage($image,$folder)
+    {
+        $valid_mime = ['image/svg+xml'];
+        $img_mime = $image['type'];
+
+        if(!in_array($img_mime,$valid_mime))
+        {
+            return 'inv_img'; //invalid image mime or format
+        } 
+        else if(($image['size']/(1024*1024))>1)
+        {
+            return 'inv_size'; //invalid size greater than 1mb
+        }
+        else
+        {
+            $ext = pathinfo($image['name'],PATHINFO_EXTENSION);
+            $rname = 'IMG_'.random_int(11111,99999).".$ext";
+
+            $img_path = UPLOAD_IMAGE_PATH.$folder.$rname;
+            if(move_uploaded_file($image['tmp_name'],$img_path)){
+                return $rname;
+            }
+            else{
+                return 'upd_failed';
+            }
+        }
+    }
+
 ?>
