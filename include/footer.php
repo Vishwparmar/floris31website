88df1c244661374fb806c7d12b4f8d8ccbd2ff7a
nupur0309 <nupur.cnj@gmail.com>
2024-01-08 22:22:18 +0530

    userregister_form

--- include/footer.php
+++ include/footer.php
@@ -38,6 +38,24 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
 <script>
+
+        function alert(type,msg){
+            let bs_class = (type == 'success') ? 'alert-success' : 'alert-danger';
+            let element = document.createElement('div');
+            element.innerHTML = `<div class="alert ${bs_class} alert-dismissible fade show custom-alert" role="alert">
+                    <strong class="me-3">${msg}</strong>                 
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>`;
+            document.body.append(element);
+            setTimeout(remAlert, 3000);
+    }
+
+    function remAlert(){
+        document.getElementsByClassName('alert')[0].remove();
+    }
+
+
+
     function setActive() 
     {
         let navbar = document.getElementById('#nav-bar');
@@ -78,9 +96,31 @@
         let xhr = new XMLHttpRequest();
         xhr.open("POST", "ajax/login_register.php", true);
         xhr.onload = function(){
+            if(this.responseText == 'pass_mismatch'){
+                alert('error', "Password Mismatch!");
+            }
+            else if(this.responseText == 'email_already'){
+                alert('error',"Email is already registered!");
+            }
+            else if(this.responseText == 'phone_already'){
+                alert('error', "Phone number is already registered!");
+            }
+            else if(this.responseText == 'inv_img'){
+                alert('error', "Only JPG, WEBP & PNG images are allowed!");
+            }
+            else if(this.responseText == 'upd_failed'){
+                alert('error', "Image upload failed!");
+            }
+            else if(this.responseText == 'ins_failed'){
+                alert('error',"Registration failed!", "Server Down!");
+            }
+            else{
+                alert('Success', "Registration successfully registered!");
+                register_form.reset();
+            }
             
         }
-
+                
         xhr.send(data);
     });
 
