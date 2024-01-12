0464a1597affcf4f8a87b2195d34e79591966c7b
shah-dishaa <dishashah071@gmail.com>
2024-01-12 20:40:18 +0530

    room_backend part1 done

--- admin/ajax/rooms.php
+++ admin/ajax/rooms.php
@@ -65,7 +65,7 @@
                $status = "<button onclick='toggle_status($row[id],0)' class='btn btn-dark btn-sm shadow-none'>active</button>"; 
             }
             else{
-                $status = "<button onclick='toggle_status($row[id],0)' class='btn btn-warning btn-sm shadow-none'>inactive</button>";
+                $status = "<button onclick='toggle_status($row[id],1)' class='btn btn-warning btn-sm shadow-none'>inactive</button>";
             }
 
             $data.="
@@ -84,13 +84,104 @@
                     <td>₹$row[price]</td>
                     <td>$row[quantity]</td>
                     <td>$status</td>
-                    <td>Buttons</td>
+                    <td>
+                        <button type='button' onclick='edit_details($row[id])' class='btn btn-primary shadow-none btn-sm' data-bs-toggle='modal' data-bs-target='#edit-room'>
+                            <i class='bi bi-pencil-square'></i> 
+                        </button>
+                    </td>
                 </tr>
             ";
             $i++;
         }
         echo $data;
     }
+
+    if(isset($_POST['get_room']))    
+    {
+        $frm_data = filteration($_POST);
+
+        $res1 = select("SELECT * FROM `rooms` WHERE `id`=?",[$frm_data['get_room']],'i');
+        $res2 = select("SELECT * FROM `room_features` WHERE `room_id`=?",[$frm_data['get_room']],'i');
+        $res3 = select("SELECT * FROM `room_facilities` WHERE `room_id`=?",[$frm_data['get_room']],'i');
+
+        $roomdata = mysqli_fetch_assoc($res1);
+        $features = [];
+        $facilities = [];
+
+        if(mysqli_num_rows($res2)>0){
+            while($row = mysqli_fetch_assoc($res2)){
+                array_push($features,$row['features_id']);
+            }
+        }
+        if(mysqli_num_rows($res3)>0){
+            while($row = mysqli_fetch_assoc($res3)){
+                array_push($facilities,$row['facilities_id']);
+            }
+        }
+
+        $data = ["roomdata" => $roomdata, "features" => $features, "facilities" => $facilities];
+
+        $data = json_encode($data);
+
+        echo $data;
+    }
+
+    if(isset($_POST['edit_room']))
+    {
+        $features = filteration(json_decode($_POST['features']));
+        $facilities = filteration(json_decode($_POST['facilities']));
+
+        $frm_data = filteration($_POST);
+        $flag = 0;
+
+        $q1 = "UPDATE `rooms` SET `name`=?,`area`=?,`price`=?,`quantity`=?,`adult`=?,`children`=?,`description`=?,`status`=? WHERE `id`=?";
+        $values = [$frm_data['name'],$frm_data['area'],$frm_data['price'],$frm_data['quantity'],$frm_data['adult'],$frm_data['children'],$frm_data['desc'],$frm_data['room_id']];
+
+        if(update($q1,$values,'siiiiisi')){
+            $flag = 1;
+        }
+
+        $del_features = delete("DELETE FROM `room_features` WHERE `room_id`=?",[$frm_data['room_id']],'i');
+        $del_facilities = delete("DELETE FROM `room_facilities` WHERE `room_id`=?",[$frm_data['room_id']],'i');
+
+        if(!($del_facilities && $del_features)){
+            $flag = 0;
+        }
+
+        $q2 = "INSERT INTO `room_facilities`(`room_id`, `facilities_id`) VALUES (?,?)";
+        
+        if($stmt = mysqli_prepare($con,$q2)){
+            foreach($facilities as $f){
+                mysqli_stmt_bind_param($stmt,'ii',$frm_data['room_id'],$f);
+                mysqli_stmt_execute($stmt);
+            }
+            $flag = 1;
+            mysqli_stmt_close($stmt);
+        } else{
+            $flag = 0;
+            die('query cannot be prepared - insert');
+        }
+
+        $q3 = "INSERT INTO `room_features`(`room_id`, `features_id`) VALUES (?,?)";
+        
+        if($stmt = mysqli_prepare($con,$q2)){
+            foreach($features as $f){
+                mysqli_stmt_bind_param($stmt,'ii',$frm_data['room_id'],$f);
+                mysqli_stmt_execute($stmt);
+            }
+            $flag = 0;
+            mysqli_stmt_close($stmt);
+        } else{
+            $flag = 1;
+            die('query cannot be prepared - insert');
+        }
+
+        if($flag){
+            echo 1;
+        } else{
+            echo 0;
+        }
+    } 
 
     if(isset($_POST['toggle_status']))
     {
